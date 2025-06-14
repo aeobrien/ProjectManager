e1c6b87f882d2ee5044ff9deee1662158979deb0
Aidan O'Brien <aidan.e.obrien@gmail.com>
2025-06-14 15:04:07 +0100

    Initial Commit

---
+++ ProjectManager/ProjectManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerApp.swift
+//  ProjectManager
+//
+//  Created by Aidan O'Brien on 14/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct ProjectManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ProjectManagerTests/ProjectManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectManagerTests.swift
+//  ProjectManagerTests
+//
+//  Created by Aidan O'Brien on 14/06/2025.
+//
+
+import Testing
+@testable import ProjectManager
+
+struct ProjectManagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
